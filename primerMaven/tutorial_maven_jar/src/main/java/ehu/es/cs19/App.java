6baf7debc4ef4ccb2dfd99b414f713706bb838bc
KarlyHdzC <34174304+KarlyHdzC@users.noreply.github.com>
2019-03-29 14:07:14 -0600

    Add files via upload

---
+++ primerMaven/tutorial_maven_jar/src/main/java/ehu/es/cs19/App.java
@@ -0,0 +1,13 @@
+package ehu.es.cs19;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! c;" );
+    }
+}
